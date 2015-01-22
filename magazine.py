fa4f1b71f332fc40a9a19a5205d3e1f7fa0cf3c8
quadrismegistus <ryan.heuser@gmail.com>
2015-01-21 18:49:30 -0800

    changed url

--- magazine.py
+++ magazine.py
@@ -1,3 +1,4 @@
-url_esquire = 'https://docs.google.com/spreadsheets/d/1V1_nrdl-UpVEVhS1zFsSIvJzA8qb1VgZm3hRdb3v1QM/pubhtml?gid=0&single=true'
+url = 'https://docs.google.com/spreadsheets/d/1V1_nrdl-UpVEVhS1zFsSIvJzA8qb1VgZm3hRdb3v1QM/pubhtml'
 
-url_elle = 'https://docs.google.com/spreadsheets/d/1V1_nrdl-UpVEVhS1zFsSIvJzA8qb1VgZm3hRdb3v1QM/pubhtml?gid=1548357702&single=true'
+import pytxt
+print pytxt.tsv2ld(url)
