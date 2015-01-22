35a815be582c1d5868d619fa76c381f804d4d71c
quadrismegistus <ryan.heuser@gmail.com>
2015-01-21 18:41:55 -0800

    added urls

--- magazine.py
+++ magazine.py
@@ -1,1 +1,3 @@
-print 'hello world'
+url_esquire = 'https://docs.google.com/spreadsheets/d/1V1_nrdl-UpVEVhS1zFsSIvJzA8qb1VgZm3hRdb3v1QM/pubhtml?gid=0&single=true'
+
+url_elle = 'https://docs.google.com/spreadsheets/d/1V1_nrdl-UpVEVhS1zFsSIvJzA8qb1VgZm3hRdb3v1QM/pubhtml?gid=1548357702&single=true'
